29c9a233128a3d384d12f0392cdab9d3e7c5e2fd
Buddy Lindsey, Jr <buddylindsey@users.noreply.github.com>
2024-02-23 21:55:24 -0600

    Add keymap to clear search highlighting

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,5 @@ local keymap = vim.api.nvim_set_keymap
 keymap("", ",", "<Noop>", opts)
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
+
+keymap("n", "<leader>cs", ":noh<CR>", opts)
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,8 +7,8 @@ end
 lualine.setup {
     options = {
         theme = "auto",
-        section_separators = {"", ""},
-        component_separators = {"", ""},
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
         icons_enabled = true,
         refresh = {
             statusline = 500,
@@ -17,18 +17,18 @@ lualine.setup {
         }
     },
     sections = {
-        lualine_a = {"mode"},
-        lualine_b = {"branch"},
-        lualine_c = {"filename"},
-        lualine_x = {"encoding", "fileformat", "filetype"},
-        lualine_y = {"progress"},
-        lualine_z = {"location"},
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename", "searchcount" },
+        lualine_x = { "encoding", "fileformat", "filetype", "tabs" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
     },
     inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {"filename"},
-        lualine_x = {"location"},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
         lualine_y = {},
         lualine_z = {},
     },
