fb0e5828a7f7e016ded4135ce148c3e721fd5891
Buddy Lindsey, Jr <buddy.lindsey@gemini.com>
2024-03-06 14:28:19 -0600

    Add floating window util and command. Then open up oil with keymap and command

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,11 @@
+local utils = require('utils')
+
+-- Add comamnd for opening a floating window
+vim.api.nvim_create_user_command('OpenFloatCmd', function(input)
+    utils.open_command_in_float(input.args)
+end, {nargs = 1})
+
+
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
@@ -7,3 +15,6 @@ vim.g.maplocalleader = ","
 
 keymap("n", "<leader>cs", ":noh<CR>", opts)
 
+vim.keymap.set('n', '<leader>o', function()
+    vim.cmd('OpenFloatCmd Oil')
+end, {noremap = true, silent = true, desc = "Open Oil in a floating window"})
--- lua/utils.lua
+++ lua/utils.lua
@@ -12,4 +12,34 @@ function M.check_app_exist(command, text)
     end
 end
 
+-- Add window for random commands
+function open_command_in_float(cmd)
+    -- Calculate the floating window size and position
+    local width = math.floor(vim.o.columns * 0.6)
+    local height = math.floor(vim.o.lines * 0.6)
+    local col = math.floor((vim.o.columns - width) / 2)
+    local row = math.floor((vim.o.lines - height) / 2)
+
+    -- Create a buffer for the floating window
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    -- Define the floating window options
+    local opts = {
+        style = "minimal",
+        relative = "editor",
+        width = width,
+        height = height,
+        col = col,
+        row = row,
+        border = "rounded",
+    }
+
+    -- Create the floating window
+    local win = vim.api.nvim_open_win(buf, true, opts)
+
+    vim.api.nvim_buf_call(buf, function()
+        vim.cmd(cmd)
+    end)
+end
+
 return M
