36b1af54de1576a4a1b60618c87b9f79fe5ebbd1
Buddy Lindsey, Jr <buddy.lindsey@gemini.com>
2024-04-25 17:02:45 -0500

    consolidate some keymaping code

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,18 +1,5 @@
 local utils = require('utils')
 
--- Add comamnd for opening a floating window
-vim.api.nvim_create_user_command('OpenFloatCmd', function(input)
-    utils.open_command_in_float(input.args)
-end, {nargs = 1})
-
-vim.api.nvim_create_user_command('OpenFloatTerm', function(input)
-    utils.open_terminal_command_in_float(input.args)
-end, {nargs = 1})
-
-vim.api.nvim_create_user_command('Yazi', function(input)
-    utils.open_terminal_command_in_float(input.args)
-end, {nargs = 1})
-
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
@@ -20,28 +7,32 @@ keymap("", ",", "<Noop>", opts)
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
+local function createOpenFloatCommand(name, func)
+    vim.api.nvim_create_user_command(name, function(input)
+        func(input.args)
+    end, {nargs = 1})
+end
+createOpenFloatCommand("OpenFloatCmd", utils.open_command_in_float)
+createOpenFloatCommand("OpenFloatTerm", utils.open_terminal_command_in_float)
+
+local function keymapFloatTerm(key, cmd, args, desc)
+    vim.keymap.set('n', key, function()
+        vim.cmd(cmd .. " " .. args)
+    end, {noremap = true, silent = true, desc = desc})
+end
+keymapFloatTerm("<leader>g", "OpenFloatTerm", "lazygit", "Open lazygit in a floating window")
+keymapFloatTerm("<leader>m", "OpenFloatTerm", "yazi", "Open Yazi file manager in a floating window")
+
 keymap("n", "<leader>cs", ":noh<CR>", opts)
 keymap("n", "<leader>tn", ":tabn<CR>", opts)
 keymap("n", "<leader>tp", ":tabp<CR>", opts)
 keymap("n", "<leader>tc", ":tabe<CR>", opts)
 
-vim.keymap.set('n', '<leader>o', function()
-    vim.cmd('OpenFloatCmd Oil')
-end, {noremap = true, silent = true, desc = "Open Oil in a floating window"})
-
-vim.keymap.set('n', '<leader>g', function()
-    vim.cmd('OpenFloatTerm lazygit')
-end, {noremap = true, silent = true, desc = "Open Oil in a floating window"})
-
-vim.keymap.set('n', '<leader>m', function()
-    vim.cmd('OpenFloatTerm yazi')
-end, {noremap = true, silent = true, desc = "Open Yazi file manager"})
-
 local function pyFmt()
     if vim.bo.filetype == "python" then
-        print('Formatting python code')
-        vim.cmd('Neoformat black')
-        vim.cmd('Neoformat isort')
+        print("Formatting python code")
+        vim.cmd("Neoformat black")
+        vim.cmd("Neoformat isort")
     end
 end
 
