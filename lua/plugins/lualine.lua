aa4868e6fb54cc6781cc6cec94b557e343f6fcf2
Buddy Lindsey, Jr <buddylindsey@users.noreply.github.com>
2024-08-11 13:12:53 -0500

    Update status line and packages

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -19,7 +19,7 @@ lualine.setup {
     sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename", "searchcount" },
+        lualine_c = { {"filename", path=1}, "searchcount" },
         lualine_x = { "encoding", "fileformat", "filetype", "tabs" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
